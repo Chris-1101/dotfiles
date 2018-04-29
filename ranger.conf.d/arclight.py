d3e7254f42e6df4c1e052cabc5f102035d83dd32
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-04-30 00:54:11 +0200

    Clean up import statements

--- ranger.conf.d/arclight.py
+++ ranger.conf.d/arclight.py
@@ -1,9 +1,18 @@
+#                         __   __        __     __
+#   _____ _______   ____ |  | |__| ____ |  |___/  |_
+#   \__  \\_  __ \_/ ___\|  | |  |/ ___\|  |  \   __\
+#    / __ \|  | \/\  \___|  |_|  / /_/  >   Y  \  |
+#   (____  /__|    \___  >____/__\___  /|___|  /__|
+#        \/            \/       /_____/      \/
+
+#?!:$HOME/.config/ranger/colorschemes/arclight.py
+
 from __future__ import (absolute_import, division, print_function)
 
 from ranger.colorschemes.default import Default
-from ranger.gui.color import cyan, magenta, blue, green
+from ranger.gui.color import blue
 
-
+# Use Default Scheme
 class Scheme(Default):
 
     def use(self, context):
