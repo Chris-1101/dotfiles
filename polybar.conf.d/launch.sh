2b701da10f9d09a51b2869d73055de2ce94b9652
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-04-30 00:42:01 +0200

    Add origin comment

--- polybar.conf.d/launch.sh
+++ polybar.conf.d/launch.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env sh
 
+#    __                              __      ___
+#   |  | _____   __ __  ____   ____ |  |__   \_ |__ _____ _______  ______
+#   |  | \__  \ |  |  \/    \_/ ___\|  |  \   | __ \\__  \\_  __ \/  ___/
+#   |  |__/ __ \|  |  /   |  \  \___|   Y  \  | \_\ \/ __ \|  | \/\___ \
+#   |____(____  /____/|___|  /\___  >___|  /  |___  (____  /__|  /____  >
+#             \/           \/     \/     \/       \/     \/           \/
+
+#?!:$HOME/.config/polybar/launch.sh
+
 # Terminate any running instances of polybar
 killall -q polybar
 
-# Wait for the above command to complete
-while pgrep -u $UID -x polybar > /dev/null
-do
+# Wait for polybar instances to be killed
+while pgrep -u $UID -x polybar > /dev/null; do
   sleep 1
 done
 
@@ -18,5 +26,3 @@ ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-btmbar
 polybar systray &
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-systray
 #ehco cmd:hide > /tmp/ipc-systray
-
-echo "Polybar loaded"
