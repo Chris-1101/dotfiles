fbe01fe3f7ae58bd6e8bdc6557137060cf84ebfe
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-04-30 00:53:36 +0200

    Update shebang to use env path

--- polybar.conf.d/updates-arch.sh
+++ polybar.conf.d/updates-arch.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
+#                         __                        ___       __
+#   _____ _______   ____ |  |__    __ ________   __| _/____ _/  |_  ____   ______
+#   \__  \\_  __ \_/ ___\|  |  \  |  |  \____ \ / __ |\__  \\   __\/ __ \ /  ___/
+#    / __ \|  | \/\  \___|   Y  \ |  |  /  |_> > /_/ | / __ \|  | \  ___/ \___ \
+#   (____  /__|    \___  >___|  / |____/|   __/\____ |(____  /__|  \___  >____  >
+#        \/            \/     \/        |__|        \/     \/          \/     \/
+
+#?!:$HOME/.config/polybar/updates-arch.sh
+
+# List Pending Updates
 updates=$(checkupdates | wc -l)
 
+# Print Number of Updates
 if [ "$updates" -gt 0 ]; then
     echo "$updates"
 else
