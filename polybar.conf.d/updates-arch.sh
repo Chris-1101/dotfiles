62618f19d484cd9032496b92425c662e5ababb92
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-05-14 19:07:44 +0200

    Simplify updates script

--- polybar.conf.d/updates-arch.sh
+++ polybar.conf.d/updates-arch.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 #                         __                        ___       __
 #   _____ _______   ____ |  |__    __ ________   __| _/____ _/  |_  ____   ______
 #   \__  \\_  __ \_/ ___\|  |  \  |  |  \____ \ / __ |\__  \\   __\/ __ \ /  ___/
@@ -9,12 +8,6 @@
 
 #?!:$HOME/.config/polybar/updates-arch.sh
 
-# List Pending Updates
-updates=$(checkupdates | wc -l)
-
-# Print Number of Updates
-if [ "$updates" -gt 0 ]; then
-    echo "$updates"
-else
-    echo "0"
-fi
+# Updates: Arch Official Repositories
+updates_aor=$(checkupdates | wc -l) || "0"
+echo "$updates_aor"
