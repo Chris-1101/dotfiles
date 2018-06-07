37a93eb5cbc96b3ea39cc7587974bf2dc5b3b54c
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-06-07 23:04:28 +0200

    Block battery level notifications on AC

--- scripts/powerctl.sh
+++ scripts/powerctl.sh
@@ -11,58 +11,77 @@
 # Author       : Chris-1101
 # GitHub       : https://github.com/Chris-1101
 # Description  : Monitor, and notify of, changes related to the power supply
-# Dependencies : dunstify
+# Dependencies : acpi, dunstify
+
+# NOTE: This script is designed with automation in mind via acpi events and/or systemd timers (or equivalent)
 
 # ==============================
 # ------- 𝙎𝙘𝙧𝙞𝙥𝙩 𝙊𝙥𝙩𝙞𝙤𝙣𝙨 -------
 # ==============================
+# The name of the script as it appears on notifications
+script_name="Power Control"
+
 # At what battery percentage should a notification be displayed
 bat_low=10
 
 # At what battery percentage should action be taken to preserve system state
 bat_critical=5
 
-# What action should be taken? Must be valid systemctl command [suspend/hibernate]
-bat_action="suspend"
+# What action should be taken? Must be a valid systemctl command [suspend/hibernate]
+sys_action="suspend"
 
-# Screen brightness to set on ac/bat swap
+# Screen brightness to set on ac/battery swap
 bl_ac=50
 bl_bat=30
+
+# ID used for notifications, can be ignored
+not_id=90271
 
 # ===================================
 # ------- 𝙁𝙪𝙣𝙘𝙩𝙞𝙤𝙣 𝘿𝙚𝙛𝙞𝙣𝙞𝙩𝙞𝙤𝙣𝙨 -------
 # ===================================
-# Get Battery Level
-function get_bat_pct
+# Get Battery Info
+function get_bat_info
 {
-    local bat_id=$(grep '^BAT' <<< $(ls /sys/class/power_supply))
-    local bat_path="/sys/class/power_supply/$bat_id"
+    local method="/sys/class/power_supply"
+    local bat_id=$(grep '^BAT' <<< $(ls $method))
+    local bat_path="$method/$bat_id"
+    local bat_status=$(cat $bat_path/status)
     local bat_pct=$(cat $bat_path/capacity)
 
-    echo $bat_pct
+    [[ $1 = "status" ]] && echo $bat_status
+    [[ $1 = "pct" ]] && echo $bat_pct
 }
 
 # Check Battery Thresholds
-function check_bat_pct
+function check_bat
 {
-    # local bat_level=`get_bat_pct`
-    local bat_level=10
-    local bat_time=$(awk -F '[\s,]' '{print $4}' <<< $(acpi -b))
+    function is_discharging
+    {
+        test `get_bat_info status` = "Discharging"
+    }
+
+    ! is_discharging && exit 0
+
+    local bat_level=`get_bat_info pct`
+    local bat_time=$(awk -F '[, ]' '{print $7, $8}' <<< $(acpi -b))
+    local lf=$(echo -e "\n(")
 
     if [[ $bat_level -le $bat_critical ]]; then
-        systemctl $bat_action
+        systemctl $sys_action
     elif [[ $bat_level -le $bat_low ]]; then
-        dunstify "Power Control" "Battery critically low: ${bat_level}% (${bat_time//$' remaining'})" -u critical -t 0 -r 90271
+        dunstify "$script_name" "Battery Critically Low: ${bat_level}%${lf}${bat_time})" \
+        -u critical -t 0 -r $not_id
     fi
 }
 
 # AC ➤ Battery
 function ac_unplugged
 {
-    local bat_level=`get_bat_pct`
+    local bat_level=`get_bat_info pct`
     local message=$(echo -e "A/C Supply Unplugged\nBattery is at")
 
-    dunstify "Power Control" "$message ${bat_level}%" -u critical -t 7000 -r 90271
+    dunstify "$script_name" "$message ${bat_level}%" -u critical -t 7000 -r $not_id
     light -S $bl_bat
 }
 
@@ -71,7 +90,7 @@ function ac_pluggedin
 {
     local message="A/C Supply Plugged In"
 
-    dunstify "Power Control" "$message" -u critical -t 7000 -r 90271
+    dunstify "$script_name" "$message" -u critical -t 7000 -r $not_id
     light -S $bl_ac
 }
 
@@ -93,7 +112,7 @@ case "$1" in
         ac_unplugged
         ;;
     low)
-        check_bat_pct
+        check_bat
         ;;
     *)
         (>&2 echo "Unknown command: $1")
