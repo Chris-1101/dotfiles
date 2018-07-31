d78433951d3ac937b51da559f11cc5c386f2cd00
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-08-01 00:31:29 +0200

    Restructure script

--- scripts/backlightctl.sh
+++ scripts/backlightctl.sh
@@ -6,48 +6,75 @@
 #    |___  (____  /\___  >__|_ \____/__\___  /|___|  /__|  \___  >__| |____/
 #        \/     \/     \/     \/      /_____/      \/          \/
 
-#!!:/usr/bin/backlightctl
+# install:set type=root path=/usr/bin/backlightctl
 
 # Author       : Chris-1101
 # GitHub       : https://github.com/Chris-1101
 # Description  : Control screen brightness and notify changes
 # Dependencies : light, dunstify
 
-# Manage Parameters
+# ======================
+# ------- 𝘾𝙤𝙣𝙛𝙞𝙜 -------
+# ======================
+# The name of the script as it appears on notifications
+script_name="Backlight Control"
+
+# Symbols used to build the notification bar
+symbol_active="■"
+symbol_empty="□"
+
+# Duration to display the notification in ms
+not_duration=3000
+
+# ID used for notifications, can be ignored
+not_id=8171
+
+# ===========================
+# ------- 𝙈𝙖𝙞𝙣 𝙎𝙘𝙧𝙞𝙥𝙩 -------
+# ===========================
+function display_notification
+{
+  # Current Brightness Level
+  bl_pct=$(awk '{$i = int( ($i + 2) / 5) * 5 ""} 1' <<< "$(light -G)")
+  bl_val=$(( $bl_pct / 10 ))
+
+  # Build Notification Bar - □ ▣ ■
+  for (( i = 1; i <= 10; i++ )); do
+    if [[ $i -le $bl_val ]]; then
+      bl_bar+="$symbol_active "
+    else
+      bl_bar+="$symbol_empty "
+    fi
+  done
+
+  # Forward to Notification Daemon
+  dunstify "$script_name" "$bl_bar" -t $not_duration -r $not_id
+}
+
+# =======================================
+# ------- 𝙋𝙖𝙧𝙖𝙢𝙚𝙩𝙚𝙧 𝙈𝙖𝙣𝙖𝙜𝙚𝙢𝙚𝙣𝙩 -------
+# =======================================
 case "$1" in
-    "")
-        printf "Missing argument - "
-        ;&
-    -h|--help)
-        printf "usage: $(basename $0) [dec/inc/get]\n"
-        exit 0
-        ;;
-    dec)
-        light -U 10
-        ;;
-    inc)
-        light -A 10
-        ;;
-    get)
-        ;;
-    *)
-        (>&2 echo "Unknown command: $1")
-        exit 1
-        ;;
+  "")
+    printf "Missing argument - "
+    ;&
+  -h|--help)
+    printf "usage: $(basename $0) [dec/inc/get]\n"
+    exit 0
+    ;;
+  dec)
+    light -U 10
+    ;;
+  inc)
+    light -A 10
+    ;;
+  get)
+    ;;
+  *)
+    (>&2 echo "Unknown command: $1")
+    exit 1
+    ;;
 esac
 
-# Determine Current Brightness Level / 10
-bl_current=$(awk '{$i = int( ($i + 2) / 5) * 0.5 ""} 1' <<< "$(light -G)")
-
-# Build Brightness Bar - □ ▣ ■
-for (( i = 1; i <= 10; i++ )); do
-    if [[ $i -le $bl_current ]]; then
-        bl_bar+="■ "
-    else
-        bl_bar+="□ "
-    fi
-done
-
-# Forward to Notification Daemon
-dunstify "Backlight Control" "$bl_bar" -t 3000 -r 8171
-unset bl_bar
+display_notification
+unset display_notification
