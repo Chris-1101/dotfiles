d90b12c982180ba987d496f91059d4c5557653a2
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-08-25 23:48:17 +0200

    Modify launch parameters

--- i3-gaps.conf.d/restore-ws.sh
+++ i3-gaps.conf.d/restore-ws.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
+#    __________        __      __             __
+#   |__\_____  \      /  \    /  \___________|  | __ _________________   _____  ____   ______
+#   |  | _(__  < _____\   \/\/   /  _ \_  __ \  |/ //  ___/\____ \__  \ /  ___\/ __ \ /  ___/
+#   |  |/       \ _____\        (  (_) )  | \/    < \___ \ |  |_) ) __ \\  \__\  ___/ \___ \
+#   |__/______  /       \__/\  / \____/|__|  |__|_ \____  >|   __(____  /\___  >___  >____  >
+#             \/             \/                   \/    \/ |__|       \/     \/    \/     \/
 
-#    __________                           __
-#   |__\_____  \     __  _  _____________|  | __ _________________    ____  ____   ______
-#   |  | _(__  < ____\ \/ \/ /  _ \_  __ \  |/ //  ___/\____ \__  \ _/ ___\/ __ \ /  ___/
-#   |  |/       \_____\     (  <_> )  | \/    < \___ \ |  |_> > __ \\  \__\  ___/ \___ \
-#   |__/______  /      \/\_/ \____/|__|  |__|_ \____  >|   __(____  /\___  >___  >____  >
-#             \/                              \/    \/ |__|       \/     \/    \/     \/
-
-#?!:$HOME/.config/i3/restore-ws.sh
+# install:set type=user path=$HOME/.config/i3/restore-ws.sh
 
 # Launch Workspace 2
 i3-msg "workspace 2; append_layout ~/.config/i3/workspace-2.json"
 
 # Restore Workspace 2
 urxvt -title 'Ranger File Manager' -name ranger -e $SHELL -c "ranger && $SHELL" &
-urxvt -name urxvt1 &
+urxvt -name urxvt1 -e $SHELL -c "lolcat <(boxecho <(quotegen)) && $SHELL" &
 urxvt -name urxvt2 -e $SHELL -c "neofetch && dfc -dfs && echo && $SHELL" &
 urxvt -title 'System Monitor' -name htop -e $SHELL -c "htop && $SHELL" &
