022227b83237d77631f16c493513e839184fe5af
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-08-27 01:32:22 +0200

    Fix urxvt1 redirects

--- i3-gaps.conf.d/restore-ws.sh
+++ i3-gaps.conf.d/restore-ws.sh
@@ -13,6 +13,6 @@ i3-msg "workspace 2; append_layout ~/.config/i3/workspace-2.json"
 
 # Restore Workspace 2
 urxvt -title 'Ranger File Manager' -name ranger -e $SHELL -c "ranger && $SHELL" &
-urxvt -name urxvt1 -e $SHELL -c "lolcat <(boxecho <(quotegen)) && $SHELL" &
+urxvt -name urxvt1 -e $SHELL -c "lolcat < <(boxecho < <(quotegen)) && $SHELL" &
 urxvt -name urxvt2 -e $SHELL -c "neofetch && dfc -dfs && echo && $SHELL" &
 urxvt -title 'System Monitor' -name htop -e $SHELL -c "htop && $SHELL" &
