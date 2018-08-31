5d3ce0dad4e8a00e83dad503324581f7bffb8ab0
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-01 00:10:14 +0200

    Set new location for cache file

--- runcom/dirnav.zsh
+++ runcom/dirnav.zsh
@@ -1,8 +1,8 @@
 #        ___ __
-#     __| _/|__|______  ____ _____ ___  __
-#    / __ | |  \_  __ \/    \\__  \\  \/ /
-#   / /_/ | |  ||  | \/   |  \/ __ \\   /
-#   \____ | |__||__|  |___|  (____  /\_/   - shortcuts to facilitate directory navigation
+#     __| _/|__|______  ____ _____ ___  ___
+#    / __ | |  \_  __ \/    \\__  \\  \/  /
+#   / /_/ | |  ||  | \/   |  \/ __ \\    /
+#   \____ | |__||__|  |___|  (____  /\__/   - shortcuts to facilitate directory navigation
 #        \/                \/     \/
 
 # atom:set grammar=sh
@@ -10,15 +10,15 @@
 # Back & Print Directory Stack
 function dir-back
 {
-    popd
-    zle reset-prompt
+  popd
+  zle reset-prompt
 }
 
 # Up & Push Directory to Stack
 function dir-parent
 {
-    pushd ..
-    zle reset-prompt
+  pushd ..
+  zle reset-prompt
 }
 
 # Save Location
@@ -27,12 +27,12 @@ save_loc=~/.cache/goto_dir
 # Save Working Directory
 function dir-save
 {
-    pwd > $save_loc
+  pwd > $save_loc
 }
 
 # Go to Saved Directory
 function dir-goto
 {
-    pushd "$(cat $save_loc)"
-    zle reset-prompt
+  pushd "$(cat $save_loc)"
+  zle reset-prompt
 }
