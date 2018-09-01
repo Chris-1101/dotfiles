89c21a9819a16615ece9cb2eb8dc0b295130caa5
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-01 23:06:25 +0200

    Update headers

--- polybar.conf.d/updates-aur.sh
+++ polybar.conf.d/updates-aur.sh
@@ -2,11 +2,11 @@
 #                                           ___       __
 #   _____   __ _________   __ ________   __| _/____ _/  |_  ____   ______
 #   \__  \ |  |  \_  __ \ |  |  \____ \ / __ |\__  \\   __\/ __ \ /  ___/
-#    / __ \|  |  /|  | \/ |  |  /  |_> > /_/ | / __ \|  | \  ___/ \___ \
+#    / __ \|  |  /|  | \/ |  |  /  |_) ) /_/ | / __ \|  | \  ___/ \___ \
 #   (____  /____/ |__|    |____/|   __/\____ |(____  /__|  \___  >____  >
 #        \/                     |__|        \/     \/          \/     \/
 
-#?!:$HOME/.config/polybar/updates-aur.sh
+# install:set type=user path=$HOME/.config/polybar/updates-aur.sh
 
 # Updates: Arch User Repository (aurutils)
 updates_aur=$(aurcheck -d aur 2> /dev/null | wc -l) || "0"
