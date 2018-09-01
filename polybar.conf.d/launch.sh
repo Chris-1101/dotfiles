43b05b956a4a08624fc6327155114b2c3d97fc3e
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-01 23:12:06 +0200

    Remove unused code

--- polybar.conf.d/launch.sh
+++ polybar.conf.d/launch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 #    __                              __      ___
-#   |  | _____   __ __  ____   ____ |  |__   \_ |__ _____ _______  ______
-#   |  | \__  \ |  |  \/    \_/ ___\|  |  \   | __ \\__  \\_  __ \/  ___/
+#   |  | _____   __ __  ____  _____ |  |__   \_ |__ _____ _______  ______
+#   |  | \__  \ |  |  \/    \/  ___\|  |  \   | __ \\__  \\_  __ \/  ___/
 #   |  |__/ __ \|  |  /   |  \  \___|   Y  \  | \_\ \/ __ \|  | \/\___ \
 #   |____(____  /____/|___|  /\___  >___|  /  |___  (____  /__|  /____  >
 #             \/           \/     \/     \/       \/     \/           \/
 
-#?!:$HOME/.config/polybar/launch.sh
+# install:set type=user path=$HOME/.config/polybar/launch.sh
 
 # Terminate any running instances of polybar
 killall -q polybar
@@ -25,4 +25,3 @@ ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-btmbar
 
 polybar systray &
 ln -sf /tmp/polybar_mqueue.$! /tmp/ipc-systray
-#ehco cmd:hide > /tmp/ipc-systray
