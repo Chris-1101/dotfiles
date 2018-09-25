8304819b38c249b881f3e1200c30ae1869037079
Chris-1101 <Chris-1101@users.noreply.github.com>
2018-09-25 23:47:45 +0200

    Set new indentation

--- scripts/lockscreen.sh
+++ scripts/lockscreen.sh
@@ -7,7 +7,7 @@
 #   ███████╗╚██████╔╝╚██████╗██║  ██╗    ███████║╚██████╗██║  ██║███████╗███████╗██║ ╚████║
 #   ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═══╝
 
-#!!:/usr/bin/lockscreen
+# install:set type=root path=/usr/bin/lockscreen
 
 # Author       : Chris-1101
 # GitHub       : https://github.com/Chris-1101
@@ -21,32 +21,32 @@ logger '[ARCLIGHT] Initialising lockscreen...'
 # ==================================
 function lock
 {
-    # Colours (rrggbbaa)
-    foreground=d7d8e0ff
-    accentColour=525b81ff
-    errorColour=d23c3dff
-    transparent=00000000
+  # Colours (rrggbbaa)
+  foreground=d7d8e0ff
+  accentColour=525b81ff
+  errorColour=d23c3dff
+  transparent=00000000
 
-    # Lock Screen Background
-    bg="$HOME/Pictures/lockscreen.png"
+  # Lock Screen Background
+  bg="$HOME/Pictures/lockscreen.png"
 
-    # i3lock Script
-    i3lock \
-        -t -n -i $bg \
-        --timesize=65 \
-        --datesize=24 \
-        --timepos="x+130:h-ch-48" \
-        --datepos="tx+10:ty+35" \
-        --time-align=1 --date-align=1 \
-        --timefont='DejaVu Sans Light' \
-        --clock --datestr "Type password to unlock..." \
-        --insidecolor=$transparent --ringcolor=$foreground --line-uses-inside \
-        --keyhlcolor=$accentColour --bshlcolor=$accentColour --separatorcolor=$transparent \
-        --insidevercolor=$transparent --insidewrongcolor=$errorColour \
-        --ringvercolor=$foreground --ringwrongcolor=$foreground --indpos="x+638:h-159" \
-        --radius=22 --ring-width=3 --veriftext="" --wrongtext="" \
-        --textcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
-        --force-clock &
+  # i3lock Script
+  i3lock \
+    -t -n -i $bg \
+    --timesize=65 \
+    --datesize=24 \
+    --timepos="x+130:h-ch-48" \
+    --datepos="tx+10:ty+35" \
+    --time-align=1 --date-align=1 \
+    --timefont='DejaVu Sans Light' \
+    --clock --datestr "Type password to unlock..." \
+    --insidecolor=$transparent --ringcolor=$foreground --line-uses-inside \
+    --keyhlcolor=$accentColour --bshlcolor=$accentColour --separatorcolor=$transparent \
+    --insidevercolor=$transparent --insidewrongcolor=$errorColour \
+    --ringvercolor=$foreground --ringwrongcolor=$foreground --indpos="x+638:h-159" \
+    --radius=22 --ring-width=3 --veriftext="" --wrongtext="" \
+    --textcolor="$foreground" --timecolor="$foreground" --datecolor="$foreground" \
+    --force-clock &
 }
 
 # =======================================
@@ -54,13 +54,13 @@ function lock
 # =======================================
 function pre_lock
 {
-    pkill -u "$USER" -USR1 dunst
+  pkill -u "$USER" -USR1 dunst
 }
 
 function post_lock
 {
-    sleep 1
-    pkill -u "$USER" -USR2 dunst
+  sleep 1
+  pkill -u "$USER" -USR2 dunst
 }
 
 # ============================
@@ -68,45 +68,36 @@ function post_lock
 # ============================
 function help
 {
-    echo
-    echo "Simple lock screen script to automate the use of i3lock-color"
-    echo "https://github.com/Chris-1101/dotfiles"
-    echo
-    echo "Options:"
-    echo
-    echo "    -h --help"
-    echo "        Displays this page"
-    echo
-    echo "    -l --lock"
-    echo "        Locks the screen (default)"
-    echo
-    echo "ps: yes, these options are rather redundant at this point :p"
-    echo
+  echo
+  echo "Simple lock screen script to automate the use of i3lock-color"
+  echo "https://github.com/Chris-1101/dotfiles"
+  echo
+  echo "Options:"
+  echo
+  echo "    -h --help"
+  echo "        Displays this page"
+  echo
+  echo "    -l --lock"
+  echo "        Locks the screen (default)"
+  echo
+  echo "ps: yes, these options are rather redundant at this point :p"
+  echo
 }
 
 # ===============================
 # ------- Option Handling -------
 # ===============================
 case $1 in
-
-    "")
-
-        ;&
-
-    -l | --lock)
-
-        lock
-        ;;
-
-    -h | --help)
-
-        help
-        ;;
-
-    *)
-
-        echo "Unknown option: $1"
-        echo "Run $0 -h for help."
-        ;;
-
+  "")
+    ;&
+  -l|--lock)
+    lock
+    ;;
+  -h|--help)
+    help
+    ;;
+  *)
+    echo "Unknown option: $1"
+    echo "Run $0 -h for help."
+    ;;
 esac
