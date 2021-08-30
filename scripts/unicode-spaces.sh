794d3fc7923131ff761b0f48fd9e88a452649bf9
Chris MB <chrismb.noreply@gmail.com>
2021-08-30 09:41:01 +0000

    Add unicode spaces script
    Just a small utility script to print the different kind of spaces found in ays containing elements with regular spaces in bash scripts.

---
+++ scripts/unicode-spaces.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#                       d8,                          d8b
+#                       `8P                         88P
+#                                                  d88
+#   ?88   d8P  88bd88b   88b  d8888b  d8888b   d888888   d8888b     .d888b,?88,.d88b,  d888b8b   d8888b  d8888b .d888b,
+#   d88   88   88P' ?8b  88P d8P' `P d8P' ?88 d8P' ?88  d8b_,dP     ?8b,   `?88'  ?88 d8P' ?88  d8P' `P d8b_,dP ?8b,
+#   ?8(  d88  d88   88P d88  88b     88b  d88 88b  ,88b 88b           `?8b   88b  d8P 88b  ,88b 88b     88b       `?8b
+#   `?88P'?8bd88'   88bd88'  `?888P' `?8888P' `?88P'`88b`?888P'    `?888P'   888888P' `?88P'`88b`?888P' `?888P'`?888P'
+#                                                                           88P'
+#   Author       : Chris-1101 @ GitHub                                     d88
+#   Description  : Prints space characters for easy copying               ?8P
+#   Dependencies : none
+
+# Spaces
+printf '%s\n' \
+'Char        Name                           Description' \
+'====        ====                           ===========' \
+'» «         Hair Space                     Thinnest' \
+'» «         Thin Space                     6 Per EM' \
+'» «         Thin Space                     5 Per EM' \
+'» «         Medium Mathematical Space      4.5 Per EM' \
+'» «         Mid Space                      4 Per EM' \
+'» «         Thick Space                    3 Per EM' \
+'» «         Punctuation Space              Advance Width of Comma or Period' \
+'» «         EN Space                       Width of 1 EN' \
+'» «         Figure Space                   Width of 1 Monospaced Digit' \
+'» «         EM Space                       Width of 1 EM' \
+'»　«        Ideographic Space              Full Width of a CJK Character'
